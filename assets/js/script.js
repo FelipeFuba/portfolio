198773d07446a4792ce507fe2912fc8275be2463
FelipeFuba <felipe.mate897@gmail.com>
2021-10-13 12:44:45 -0300

    popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,33 @@ document.addEventListener('aos:in', ({ detail }) => {
   document.addEventListener('aos:out', ({ detail }) => {
     console.log('animated out', detail);
 });
-
+$(document).ready(function() {
+  $('a[name=modal]').click(function(e) {
+    e.preventDefault();
+  var id = $(this).attr('href');
   
+  var maskHeight = $(document).height();
+  var maskWidth = $(window).width();
+  
+  $('#mask').css({'width':maskWidth, 'height':maskHeight});
+  
+  $('#mask').fadeIn(1000);
+  $('#mask').fadeTo("slow", 0.8);
+  
+  var winH = $(window).height();
+  var winW = $(window).width();
+  
+  $(id).css('top', winH/2-$(id).height()/2);
+  $(id).css('left', winW/2-$(id).width()/2);
+  $(id).fadeIn(900);
+});
+
+$('.windows .close').click(function(e) {
+  e.preventDefault();
+  $('#mask, .windows').hide();
+});
+  $('#mask').click(function(){
+    $(this).hide();
+    $('.windows').hide();
+  });
+});
